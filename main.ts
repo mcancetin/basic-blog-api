5933a4b1a9ec48d375eaeb719fa1f1f8ee3abebf
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2025-10-18 15:11:15 +0300

    Update schema with posts, tags, categories tables

--- main.ts
+++ main.ts
@@ -1,37 +1,3 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { eq } from "drizzle-orm";
-import { usersTable } from "./src/db/schema.ts";
+// import { drizzle } from "drizzle-orm/node-postgres";
 
-const db = drizzle(Deno.env.get("DATABASE_URL")!);
-
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  };
-
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
-
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-}
-
-main();
+// const db = drizzle(Deno.env.get("DATABASE_URL")!);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,34 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const categories = pgTable("categories", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar({ length: 255 }).notNull().unique(),
 });
+
+export const tags = pgTable("tags", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const posts = pgTable("posts", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: varchar({ length: 255 }).notNull(),
+  content: text().notNull(),
+  categoryId: uuid("category_id").notNull().references(() => categories.id),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+});
+
+export const postsTags = pgTable("posts_tags", {
+  postId: uuid("post_id").notNull().references(() => posts.id),
+  tagId: uuid("tag_id").notNull().references(() => tags.id),
+}, (t) => [
+  primaryKey({ columns: [t.postId, t.tagId] }),
+]);
